52528669c4240a4afa99494067e1fc6260028049
Ana Markovic <markovicana@icloud.com>
2021-10-26 18:10:11 +0200

    js

---
+++ main.js
@@ -0,0 +1,42 @@
+const forkedRepositories = {
+  user: "radicbane",
+  forkName: "radicbane / loruki - website",
+  repName: "bradtraversy / loruki - website",
+  date: "3 days ago",
+  updated: "Oct 1",
+};
+
+const otherUsersRepositories = [
+  {
+    user: "radicbane",
+    repName: "radicbane/teh-page",
+    date: "3 days ago",
+    updated: "Oct 1",
+  },
+  {
+    user: "radicbane",
+    repName: "radicbane/form-page",
+    date: "7 days ago",
+    updated: "Oct 1",
+  },
+  {
+    user: "radicbane",
+    repName: "radicbane/product-page",
+    date: "11 days ago",
+    updated: "Oct 1",
+  },
+  {
+    user: "radicbane",
+    repName: "radicbane/tehnicaldoc",
+    date: "19 days ago",
+    updated: "Oct 1",
+  },
+];
+
+const myRepositories = [
+  "name: markovic-ana / product-landing-page",
+  "markovic-ana / form-page",
+  "markovic-ana / portfolio-page",
+  "markovic-ana / github-page",
+  "markovic-ana / technical-document-page",
+];
